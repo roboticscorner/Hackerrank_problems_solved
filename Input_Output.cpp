5285662bb2f67fb9e3b52f473b3f7304a7f55edc
roboticscorner <148546240+roboticscorner@users.noreply.github.com>
2023-10-20 19:38:32 +0300

    Add files via upload

---
+++ Input_Output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    
+    // Read three integers from stdin
+    cin >> a >> b >> c;
+    
+    // Calculate the sum of the three integers
+    int sum = a + b + c;
+    
+    // Print the sum to stdout
+    cout << sum << endl;
+    
+    return 0;
+}
