2e3afa182b6c6cba42d5ee95e0b7b4d261167bb9
roboticscorner <148546240+roboticscorner@users.noreply.github.com>
2023-11-02 12:35:37 +0200

    Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -1,47 +1,15 @@
-/*Functions are a bunch of statements glued together. A function is provided with zero or more arguments, and it executes the statements on it. Based on the return type, it either returns nothing (void) or something.
+#include <iostream>
+#include <algorithm>
 
-The syntax for a function is
-
-return_type function_name(arg_type_1 arg_1, arg_type_2 arg_2, ...) {
-    ...
-    ...
-    ...
-    [if return_type is non void]
-        return something of type `return_type`;
+int max_of_four(int a, int b, int c, int d) {
+    int max_num = std::max({a, b, c, d});
+    return max_num;
 }
-For example, a function to return the sum of four parameters can be written as
-*/
 
-/*
-Write a function int max_of_four(int a, int b, int c, int d) which returns the maximum of the four arguments it receives.
-
-+= : Add and assignment operator. It adds the right operand to the left operand and assigns the result to the left operand.
-a += b is equivalent to a = a + b;
-Input Format
-
-Input will contain four integers -  , one per line.
-
-Output Format
-
-Return the greatest of the four integers.
-PS: I/O will be automatically handled.
-
-Sample Input
-
-3
-4
-6
-5
-Sample Output
-
-6
-
-*/
-int sum_of_four(int a, int b, int c, int d) {
-    int sum = 0;
-    sum += a;
-    sum += b;
-    sum += c;
-    sum += d;
-    return sum;
+int main() {
+    int a, b, c, d;
+    std::cin >> a >> b >> c >> d;
+    int max = max_of_four(a, b, c, d);
+    std::cout << max << std::endl;
+    return 0;
 }
